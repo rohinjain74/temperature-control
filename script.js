7d30656fc7df52e8945732f0de8ca294a80a6e61
rohinjain74 <138163819+rohinjain74@users.noreply.github.com>
2023-09-02 22:10:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const celsiusInput = document.getElementById("celsiusInput");
+    const convertButton = document.getElementById("convertButton");
+    const result = document.getElementById("result");
+
+    convertButton.addEventListener("click", function () {
+        const celsius = parseFloat(celsiusInput.value);
+
+        if (!isNaN(celsius)) {
+            const fahrenheit = (celsius * 9/5) + 32;
+            result.textContent = `${celsius}°C is equal to ${fahrenheit.toFixed(2)}°F`;
+        } else {
+            result.textContent = "Please enter a valid number.";
+        }
+    });
+});
